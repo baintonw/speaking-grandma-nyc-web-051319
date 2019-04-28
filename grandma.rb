6ed93ee45e0358c5cc82250f1814762d9f167d77
William Bainton <baintonw@gmail.com>
2019-04-28 19:25:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,6 @@ def speak_to_grandma(shout)
   
   if "#{shout}" == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  end
   
-  elsif "#{shout}" !== "#{shout}".upcase
-    "HUH?! SPEAK UP, SONNY!"
-
 end
 
